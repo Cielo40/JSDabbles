895209966372c3e0fafa1642deb4111d85f51ef8
Cielo40 <talankerley474@gmail.com>
2019-07-10 18:36:58 +0930

    Create BMI.js
    Haven't used JS in a few year's as such creating small drabble's and challenge's to refresh myself

---
+++ BMI.js
@@ -0,0 +1,15 @@
+var issacMass, issacHeight, marcusMass, marcusHeight;
+
+issacMass = 55;
+issacHeight = 1.78;
+
+marcusMass = 80;
+marcusHeight = 1.95;
+
+
+var issacBMI = issacMass / (issacHeight * issacHeight);
+var marcusBmi = markMass / (marcusHeight * marcusHeight);
+console.log(issacBMI, marcusBmi)
+
+var higher = issacBMI >= markMass;
+console.log("Is issacs BMI higher than marcus " + higher);
